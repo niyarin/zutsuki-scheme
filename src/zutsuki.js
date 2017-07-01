fa00194e1af5103ba67955def203a3747983a4cd
niyarin <niyarium@gmail.com>
2017-07-01 09:55:31 +0900

    add error-object-generator

--- src/zutsuki.js
+++ src/zutsuki.js
@@ -101,7 +101,9 @@ Zutsuki.Inline_function = function(data){
 
 Zutsuki.Error = function(message,file,line){
     this.type = Zutsuki.TYPE_ERROR;
+    this.error_type = null;
     this.message = message;
+    this.code = null;
     this.file = file;
     this.line = line;
 }
@@ -122,3 +124,121 @@ Zutsuki.TRUE = function(data){
 Zutsuki.FALSE = function(data){
     return new Zutsuki.Boolean("#f");
 }
+
+
+
+Zutsuki.generate_error_with_hint_object = function(message,hint_object,error_type){
+    //エラーオブジェクトを生成する。
+    //Scheme Objectをhint_objectとして、エラーの位置をそこから得る.
+    
+    var line = -1;
+    var line_min = -1;
+    var line_max = -1;
+    var filename = null;
+    var hit = 0;
+
+    function search(object,searched){
+        if (object){
+            if (object.type == Zutsuki.TYPE_PAIR){
+                if (object.circle_flag){
+                    if (searched[object]){
+                        return;
+                    }
+                    searched[object] = true;
+                }
+                search(object.car,object.cdr);
+            }else{
+
+                if (object.line && object.filename){
+                    if (hit == 0){
+                        line = object.line;
+                        filename = object.filename;
+                        hit = 1
+
+                        line_min = line;
+                        line_max = line;
+                    }else if (hit == 1){
+                        if (filename != object.filename){
+                            hit == 2;
+                        }else{
+                            if (line == object.line){
+                            
+                            }else{
+                                if (Math.abs(line-object.line) == 8){
+                                    hit = 2;
+                                }
+
+                                line_min = Math.min(object.line,min_line)
+                                line_max = Math.max(object.line,max_line)
+                                line = (line_min + line_max)/2;
+                            }
+                        }
+                    }else if (hit == 2){
+                        return;
+                    }               
+                }
+                
+            }
+        }
+    }
+
+    search(hint_object,{});
+    var res = null;
+    if (line == -1){
+        res = new Zutsuki.Error(message);
+        res.code = hint_object;
+    }else if (line_min == line_max){
+        res = new Zutsuki.Error(message,filename,line);
+        res.code = hint_object;
+    }else{
+        const line_message = line_min + "-" + line_max;
+        res = new Zutsuki.Error(message,filename,line_message);
+        res.code = hint_object;
+    }
+    return res;
+}
+
+
+
+
+
+Zutsuki.printer = function(obj){
+    function loop(o,looked){
+        if (!o){
+            return "'()";
+        }
+        
+        if (o.type == Zutsuki.TYPE_PAIR){
+            var res = "";
+            if (o.circle_flag){
+                return "<CIRCLE OBJECT>";
+            }else{
+                res += "(";
+                var cell = o;
+                while (cell){
+                    if (cell.type != Zutsuki.TYPE_PAIR){
+                        res += " . ";
+                        res += loop(cell,looked);
+                        break;
+                    }
+                    res += " " + loop(cell.car,looked);
+                    cell = cell.cdr;
+                }
+                res += ")";
+                return res;
+            }
+        }else if (o.type == Zutsuki.TYPE_SYMBOL){
+            return o.data;
+        }else if (o.type == Zutsuki.TYPE_NUMBER){
+            return o.data;
+        }else{
+            return "<?OBJECT>";
+        }
+
+    }
+    return loop(obj,{});
+}
+
+
+
+
